ea3fbb80ba6f3fd344c474670e02a51500ef2beb
timocapa <timo.capa@gmail.com>
2017-07-16 18:58:56 +0200

    first preperations for the lios kernel

---
+++ anykernel2/anykernel.sh
@@ -0,0 +1,38 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+kernel.string=franco.Kernel by franciscofranco @ xda-developers
+do.devicecheck=1
+do.initd=0
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=OnePlus2
+device.name2=oneplus2
+
+# shell variables
+block=/dev/block/platform/soc.0/f9824900.sdhci/by-name/boot;
+is_slot_device=0;
+
+## end setup
+
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.qcom.rc
+insert_line init.qcom.rc "init.fk.rc" after "import init.qcom.power.rc" "import init.fk.rc";
+#replace_string init.angler.rc "#    verity_load_state" "    verity_load_state" "#    verity_load_state"
+#replace_string init.angler.rc "#    verity_update_state" "    verity_update_state" "#    verity_update_state"
+#replace_section init.angler.rc "service atfwd" "group system radio" "service atfwd /system/bin/ATFWD-daemon\n    disabled\n    class late_start\n    user system\n    group system radio";
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
---
+++ build.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+#
+# null oneplus2 build script
+#
+clear
+
+# Resources
+THREAD="-j4"
+KERNEL="Image.gz-dtb"
+DEFCONFIG="oneplus2_defconfig"
+DEVICE="oneplus2"
+
+# Kernel Details
+VARIANT=$(date +"%Y%m%d")
+export ARCH=arm64
+export SUBARCH=arm64
+export CROSS_COMPILE=${HOME}/Android/ToolChains/aarch64-linux-android-8.x/bin/aarch64-linux-android-
+
+# Paths
+KERNEL_DIR="${HOME}/Android/kernel/lios"
+ANYKERNEL_DIR="$KERNEL_DIR/anykernel2"
+MODULES_DIR="$ANYKERNEL_DIR/modules"
+ZIP_MOVE_NIGHTLY="${HOME}/Android/kernel/one_plus_2/out/$DEVICE"
+ZIMAGE_DIR="$KERNEL_DIR/arch/arm64/boot"
+
+# Functions
+function clean_all {
+		echo
+		find . -name "*~" -type f -delete
+		rm -rf $ANYKERNEL_DIR/$KERNEL
+		rm -rf $MODULES_DIR/*
+		make clean && make mrproper
+}
+
+function make_kernel {
+		echo
+		make $DEFCONFIG
+		make $THREAD
+}
+
+function make_zip {
+		cp -vr $ZIMAGE_DIR/$KERNEL $ANYKERNEL_DIR/$KERNEL
+		cd $ANYKERNEL_DIR
+		zip -r9 tocino-$DEVICE-$KERNEL_VER.zip *
+		mv tocino-$DEVICE-$KERNEL_VER.zip $ZIP_MOVE_NIGHTLY
+		cd $KERNEL_DIR
+}
+
+cat << "EOF"
+
+icon placeholder
+
+EOF
+
+echo "You are building lios kernel $KERNEL_VER for $DEVICE";
+echo
+
+while read -p "Do you want to clean stuffs (y/n)? " cchoice
+do
+case "$cchoice" in
+	y|Y )
+		clean_all
+		echo
+		echo "All Cleaned now."
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo
+
+while read -p "Do you want to build kernel (y/n)? " dchoice
+do
+case "$dchoice" in
+	y|Y)
+		DATE_START=$(date +"%s")
+		make_kernel
+		if [ -f $ZIMAGE_DIR/$KERNEL ];
+		then
+			make_zip
+		else
+			echo
+			echo "Kernel build failed."
+			echo
+		fi
+		break
+		;;
+	n|N )
+		DATE_START=$(date +"%s")
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
